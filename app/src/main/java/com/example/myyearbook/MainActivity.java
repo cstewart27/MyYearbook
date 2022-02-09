25153fb67912bcbe6fb42b83c8ad5a6a5b057f80
chris <cstewart3@student.sjcny.edu>
2022-02-08 20:01:26 -0500

    Added button to change set text in textView

--- app/src/main/java/com/example/myyearbook/MainActivity.java
+++ app/src/main/java/com/example/myyearbook/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myyearbook;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        TextView txt1 = (TextView) findViewById(R.id.txt1);
+
+        btn1.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v){
+                txt1.setText("Great job!!! You changed the text");
+
+
+            }
+        });
     }
+
 }
